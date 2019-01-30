126940824e6bade8b51059d1f45debbf8c74ceed
hdessomme <hdessomme@gmail.com>
2019-01-30 14:02:32 +0100

    Add new files following tutorial
    - Views
    - Controllers
    - Migrations

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title', 'About Us')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aspernatur cumque dolore eos error fuga fugiat fugit illo iusto minima nemo nesciunt odit provident quia quisquam, recusandae. In, ut?</p>
+@endsection
