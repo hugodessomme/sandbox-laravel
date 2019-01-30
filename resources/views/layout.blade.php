0a7670b6127b47412ae37757a3d9d1af403c424f
hdessomme <hdessomme@gmail.com>
2019-01-30 22:39:30 +0100

    Add Controllers and Models

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
+        <li><a href="/projects">Projects</a></li>
         <li><a href="/contact">Contact Us</a></li>
     </ul>
     @yield('content')
