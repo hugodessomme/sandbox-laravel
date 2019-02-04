1353dcbe8fa4a0a68f80d087b8d8f88226572165
Hugo <hdessomme49@gmail.com>
2019-02-04 19:55:28 +0100

    Add form validation

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -78,7 +78,12 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        Project::create(request(['title', 'description']));
+        Project::create(
+            request()->validate([
+                'title' => ['required', 'min:3', 'max:255'],
+                'description' => ['required', 'min:3']
+            ])
+        );
 
         return redirect('/projects');
     }
